6a7107ab1534a0cc5101abf61f75e056416cdaeb
Simon <aiqiang.fu@hp.com>
2019-11-14 14:57:22 +0800

    Create preview UI for Fortnite game.

--- Project-Aurora/Project-Aurora/Profiles/Fortnite/Control_Fortnite.xaml.cs
+++ Project-Aurora/Project-Aurora/Profiles/Fortnite/Control_Fortnite.xaml.cs
@@ -8,75 +8,27 @@ using System.Windows.Navigation;
 namespace Aurora.Profiles.Fortnite {
 
     public partial class Control_Fortnite : System.Windows.Controls.UserControl {
+        private Application m_profile;
+        private GameState_Fortnite State => m_profile.Config.Event._game_state as GameState_Fortnite;
+
         public Control_Fortnite(Application profile) {
-            //InitializeComponent();
-            //enableOsu.DataContext = profile.Settings;
+            InitializeComponent();
+            m_profile = profile;
         }
 
-        /// <summary>
-        /// Listener for a HyperLink to open the Uri it has specified.
-        /// </summary>
-        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e) {
-            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
-            e.Handled = true;
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem item = e.AddedItems[0] as ComboBoxItem;
+            string effect = item.Content.ToString();
+
+            if (!string.IsNullOrEmpty(effect))
+            {
+                Global.logger.Info(effect);
+                if(State != null)
+                {
+                    State.Game.Status = effect.ToLower();
+                }
+            }
         }
-
-        /// <summary>
-        /// Opens the OsuSync release page.
-        /// </summary>
-        private void OpenOsuSyncRepo_Click(object sender, System.Windows.RoutedEventArgs e) {
-            Process.Start(new ProcessStartInfo(@"https://github.com/OsuSync/Sync/releases"));
-        }
-
-        /// <summary>
-        /// Installs the Aurora OsuSync Plugin.
-        /// </summary>
-        private void InstallPlugin_Click(object sender, System.Windows.RoutedEventArgs e) {
-            //SelectOsuSync(dir => {
-            //    try {
-            //        using (var stream = new MemoryStream(Properties.Resources.OsuSyncAuroraPlugin))
-            //            stream.CopyTo(new FileStream(GetPluginPath(dir), FileMode.Create));
-            //        MessageBox.Show("Aurora OsuSync Plugin successfully installed.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            //    } catch (Exception ex) {
-            //        MessageBox.Show("Aurora OsuSync Plugin was not removed.\n\n" + ex.Message, "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            //    }
-            //});
-        }
-
-        /// <summary>
-        /// Uninstalls the Aurora OsuSync Plugin.
-        /// </summary>
-        private void UninstallPlugin_Click(object sender, System.Windows.RoutedEventArgs e) {
-            //SelectOsuSync(dir => {
-            //    try {
-            //        var f = GetPluginPath(dir);
-            //        if (File.Exists(f))
-            //            File.Delete(f);
-            //        MessageBox.Show("Aurora OsuSync Plugin successfully removed.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            //    } catch (Exception ex) {
-            //        MessageBox.Show("Aurora OsuSync Plugin was not removed.\n\n" + ex.Message, "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            //    }
-            //});
-        }
-
-        /// <summary>
-        /// Pops up a dialog box asking the user to select the OsuSync program.
-        /// When the user does so, it calls "then" with the OsuSync directory.
-        /// </summary>
-        private void SelectOsuSync(System.Action<string> then) {
-            //var dialog = new OpenFileDialog {
-            //    Title = "Select OsuSync program",
-            //    Filter = "OsuSync|sync.exe",
-            //    CheckFileExists = true,
-            //};
-
-            //if (dialog.ShowDialog() == DialogResult.OK)
-            //    then.Invoke(Path.GetDirectoryName(dialog.FileName));
-        }
-
-        /// <summary>
-        /// Gets the full path of the Aurora OsuSync Plugin for the given OsuSync directory.
-        /// </summary>
-        //private string GetPluginPath(string osuSyncDir) => Path.Combine(osuSyncDir, "Plugins", "OsuSyncAuroraPlugin.dll");
     }
 }
